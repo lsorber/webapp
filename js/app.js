94442abccc6a2feac839d0d216c9e3cd21173231
lsorber <laurent.sorber@gmail.com>
2017-08-25 22:23:21 +0200

    Consolidation

---
+++ js/app.js
@@ -0,0 +1,7 @@
+---
+---
+
+{% include_relative libraries/simplebar.js %}
+{% include_relative libraries/vue.min.js %}
+{% include_relative libraries/vue-resource.min.js %}
+{% include_relative libraries/vue-router.min.js %}
